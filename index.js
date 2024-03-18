da881681f9ea983b00476ce2a2052aa31757b15d
ivazquez-cmnrnk <ivazquez@cemenurnk.org.ar>
2024-03-18 14:45:29 -0300

    Eventos de los miembros del workflow y de la edición de tarjetas 18/9/2024

--- index.js
+++ index.js
@@ -38,11 +38,15 @@ io.on("connection", (socket) => {
     //Trajetas del workflow
     {name: "ondrop_sysmedi10_workflow"},
     {name: "hide_sysmedi10_workflow"},
+    {name: "update_sysmedi10_workflow"},
     //Etiquetas del workflow
     {name: "insert_sysmedi28_workflow"},
     {name: "delete_sysmedi28_workflow"},
     {name: "insert_sysmedi27_workflow"},
-    {name: "update_sysmedi27_workflow"}
+    {name: "update_sysmedi27_workflow"},
+    //Miembros del workflow
+    {name: "insert_sysmedi25_workflow"},
+    {name: "delete_sysmedi25_workflow"},
   ]
 
   //Renderizado dinámico de event listeners
