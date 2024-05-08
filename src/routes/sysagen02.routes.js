2301aad00a7eead99e929cdce92acc67fb0c78c8
ivazquez-cmnrnk <ivazquez@cemenurnk.org.ar>
2024-05-08 08:56:31 -0300

    endpoints de la tabla sys_agen_02(lista de espera) 8/5/2024

--- src/routes/sysagen02.routes.js
+++ src/routes/sysagen02.routes.js
@@ -3,11 +3,19 @@ import { io } from "../index.js"
 
 const sysAgen02Router = Router()
 
-sysAgen02Router.post("/sysagen02_llamado", (req, res) => {
-  console.log("sysagen02_llamado => ", req.body)
-  io.emit("sysagen02_llamado", req.body)
+const sysAgen02Fields = [
+  "sysagen02_llamado",
+  "sysagen02_inicio",
+  "sysagen02_finalizo"
+]
 
-  res.status(200).json({resultid: "success"})
+sysAgen02Fields.forEach(field => {
+  sysAgen02Router.post(`/${field}`, (req, res) => {
+    console.log(`${field} => `, req.body)
+    io.emit(field, req.body)
+  
+    res.status(200).json({resultid: "success"})
+  })  
 })
 
 export default sysAgen02Router
